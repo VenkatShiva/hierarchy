fb36cdd6875484a5ab60d05ead16c1ca1f4b1797
Venkatashiva Avula <venkatshiva@campk12.com>
2022-04-06 12:57:48 +0530

    setup done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+// import './app.scss'
+import { AppComp } from './app-styles';
+
+function App() {
+  return (
+    <AppComp>
+      <h1>
+        hii <span>hellow</span>
+      </h1>
+    </AppComp>
+  );
+}
+
+export default App;
---
+++ src/app-styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AppComp = styled.main`
+  width: 100%;
+  height: 100%;
+  h1 {
+    color: red;
+  }
+`;
